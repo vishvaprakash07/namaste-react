d19bfc04048465cdc8063a8cd4a7301bd1a2435b
Vishva <vishwa.muddasani@serole.com>
2025-07-17 23:12:52 +1000

    Episode 4 Building Food App foundation

--- App.js
+++ App.js
@@ -1,26 +1,65 @@
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [
-        React.createElement(
-            "div",
-            { id: "child" },
-            [ 
-                React.createElement("h1", {}, "I am an h1 tag"),
-                React.createElement("h2", {}, "I am an h2 tag"),
-            ]   
-        ),
-        React.createElement(
-            "div",
-            { id: "child2" },
-            [ 
-                React.createElement("h1", {}, "I am an h1 tag"),
-                React.createElement("h2", {}, "I am an h2 tag"),
-            ]   
-        )
-    ]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" alt="logo" src="https://i.pinimg.com/originals/3d/a0/00/3da000e71ddc31ec29da41266b182ade.jpg" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+const styleCard = {
+    
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-logo" alt="res-logo" src="https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg" />
+            <h3>Meghana Foods</h3>
+            <h4>Biryani, Asian, North Indian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
