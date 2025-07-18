73d71121b3f16474977b06b516ba6d90c12369cb
Vishva <vishwa.muddasani@serole.com>
2025-07-18 17:08:39 +1000

    Episode 4 Talk is cheap, Writing Config Driven UI, passing dynamic data into Restaurant cards

--- App.js
+++ App.js
@@ -21,18 +21,534 @@ const Header = () => {
     )
 }
 
-const styleCard = {
-    
+const resObj = {
+    info: {
+      id: "89373",
+      name: "Akshay tiffins",
+      cloudinaryImageId: "bxc32vivxysgcgpzizgu",
+      locality: "Rohini Hospital Road",
+      areaName: "Hanamkonda",
+      costForTwo: "₹100 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "28696",
+      avgRatingString: "4.5",
+      totalRatingsString: "28K+",
+      sla: {
+        deliveryTime: 13,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "10-15 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 21:15:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/akshay-tiffins-rohini-hospital-road-hanamkonda-rest89373",
+      type: "WEBLINK",
+    },
 }
 
-const RestaurantCard = () => {
+const restaurants = [
+  {
+    info: {
+      id: "89373",
+      name: "Akshay tiffins",
+      cloudinaryImageId: "bxc32vivxysgcgpzizgu",
+      locality: "Rohini Hospital Road",
+      areaName: "Hanamkonda",
+      costForTwo: "₹100 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "28696",
+      avgRatingString: "4.5",
+      totalRatingsString: "28K+",
+      sla: {
+        deliveryTime: 13,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "10-15 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 21:15:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/akshay-tiffins-rohini-hospital-road-hanamkonda-rest89373",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "99554",
+      name: "New Maruthi tiffins",
+      cloudinaryImageId: "hxoz63mldxl4iijoe7ua",
+      locality: "Circuit House Road",
+      areaName: "Hanamkonda",
+      costForTwo: "₹100 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "147252",
+      avgRatingString: "4.5",
+      totalRatingsString: "11K+",
+      sla: {
+        deliveryTime: 11,
+        lastMileTravel: 1.2,
+        serviceability: "SERVICEABLE",
+        slaString: "10-15 mins",
+        lastMileTravelString: "1.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 14:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/new-maruthi-tiffins-circuit-house-road-hanamkonda-rest99554",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "98618",
+      name: "Kadai Tiffins",
+      cloudinaryImageId: "acarfillhnx35sf3ik7a",
+      locality: "KLN Reddy Colony",
+      areaName: "Hanamkonda",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "113309",
+      avgRatingString: "4.4",
+      totalRatingsString: "6.4K+",
+      sla: {
+        deliveryTime: 12,
+        lastMileTravel: 1.3,
+        serviceability: "SERVICEABLE",
+        slaString: "10-15 mins",
+        lastMileTravelString: "1.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 13:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/kadai-tiffins-kln-reddy-colony-hanamkonda-rest98618",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "86867",
+      name: "Sri Geetha Bhavan",
+      cloudinaryImageId: "u8vikokk1nopupywmu9t",
+      locality: "Kakaji Nagar Colony",
+      areaName: "Hanamkonda",
+      costForTwo: "₹150 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.3,
+      veg: true,
+      parentId: "193720",
+      avgRatingString: "4.3",
+      totalRatingsString: "20K+",
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 1.6,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 21:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/sri-geetha-bhavan-kakaji-nagar-colony-hanamkonda-rest86867",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "693276",
+      name: "Ovenfresh Cakes And Desserts",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/4349f641-1812-4f3a-89d3-f3d39f451bc3_693276.jpg",
+      locality: "Mandi,Bazar",
+      areaName: "Warangal",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Bakery",
+        "Desserts",
+        "Beverages",
+        "Ice Cream",
+        "Sweets",
+        "Juices",
+        "Bengali",
+        "Pizzas",
+      ],
+      avgRating: 4.5,
+      parentId: "1372",
+      avgRatingString: "4.5",
+      totalRatingsString: "223",
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 6.1,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-18 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹64",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-29daee0c-ebc8-4910-bf6a-1abf0748e6c6",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/warangal/ovenfresh-cakes-and-desserts-mandi-bazar-warangal-rest693276",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    console.log(resData);
+    const { cloudinaryImageId, name, cuisines, costForTwo, avgRating, sla } = resData?.info;
     return (
         <div className="res-card">
-            <img className="res-logo" alt="res-logo" src="https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg" />
-            <h3>Meghana Foods</h3>
-            <h4>Biryani, Asian, North Indian</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 minutes</h4>
+            <img className="res-logo" alt="res-logo" 
+                            src={
+                                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+                                cloudinaryImageId
+                            }
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(', ')}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{sla.deliveryTime} minutes</h4>
         </div>
     )
 }
@@ -42,11 +558,9 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                {
+                    restaurants.map((restaurant) => (<RestaurantCard key={restaurant.info.id} resData={restaurant}/>))
+                }
             </div>
         </div>
     )
