0c1760ca4e4b7a20ab5e122147327384aab6f682
Vishva <vishwa.muddasani@serole.com>
2025-08-17 19:07:25 +1000

    ep 8 class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,29 @@
+import React from 'react';
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <h2>This is Namaste React Web series.</h2>
-            <User name="Vishva (functional)" location="Sydney functional"/>
-            <UserClass name="Vishva (class)" location="Sydney class" />
-            
-        </div>
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("parent Constructor");
+    }
 
-        
-    );
-};
+    componentDidMount() {
+        console.log("parent ComponentDidMount");
+    }
+
+    render() {
+        console.log("parent Render");
+        return (
+            <div>
+                <h1>About Us</h1>
+                <h2>This is Namaste React Web series.</h2>
+                <User name="Vishva (functional)" location="Sydney functional"/>
+                <UserClass name="Vishva (class)" location="Sydney class" />
+            </div>
+        );
+    };
+}
+
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,16 +7,29 @@ class UserClass extends React.Component {
         console.log("User Class Props",props);
         this.state = {
             count: 0,
+            count2: 0
         }
+
+        console.log("child constructor");
+    }
+
+    componentDidMount() {
+        console.log("Child componentDidMount");
     }
 
     render() {
+        console.log("Child render");
         const { name, location } =  this.props;
         const { count } = this.state;
 
         return (
             <div className="user-card">
                 <h1>Count: { count }</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1
+                    })
+                }}>Increase Count</button>
                 <h2>Name: { name }</h2>
                 <h3>Location: { location }</h3>
                 <h3>Contact: vishvaprakash07@gmail.com</h3>
